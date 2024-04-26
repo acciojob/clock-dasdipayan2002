ec1278e19828112357b73ac14290bd5583e7e9b4
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-26 20:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+let para = document.getElementById("timer");
+
+function currentTime() {
+	let date = new Date();
+	
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	minute = minute <=9 ? "0"+minute : minute;
+	let second = date.getSeconds();
+	second = second <=9 ? "0"+second : second;
+    let am_pm;
+
+	if(hour == 0){
+		hour=12;
+		am_pm = "AM";
+	}
+	else if(hour<12){
+		am_pm = "AM";
+	}
+	else if(hour == 12){
+		am_pm = "PM";
+	}
+	else if(hour < 12){
+		hour = hour-12;
+		am_pm = "PM";
+	}
+
+	let dateDay = date.getDate();
+	let month = date.getMonth()+1;
+	let year = date.getFullYear();
+
+    para.innerText = ${month}/${dateDay}/${year}, ${hour}:${minute}:${second} ${am_pm}
+}
+
+setInterval(currentTime, 1000)
